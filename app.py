a57dd2271168cba653a4f5105d7cd1e892b74b42
Kris Warner <kdwarn@gmail.com>
2018-04-27 00:07:29 -0400

    add error checking, pull some of it into function

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 '''
     TO DO:
-        - error checking on authenticate()
         - I'm not sure if I have to do the full authentication thing if a token
           expires or if there's a simple token refresh process
 '''
@@ -29,13 +28,24 @@ except EOFError:
     settings = {}
 
 
+################################################################################
 #  HELPER FUNCTIONS
+################################################################################
 def save(settings):
     ''' Save user settings to file. '''
 
     with open(user_settings, 'wb') as f:
         pickle.dump(settings, f)
 
+
+def get_data_or_raise_exception(response):
+    data = response.json()['rsp']
+
+    if data['stat'] != 'ok':
+        click.echo("There's been an error.")
+        raise Exception('{}: {}'.format({data['err']['code']}, {data['err']['msg']}))
+
+    return data
 
 def make_api_sig(params):
     '''
@@ -55,8 +65,9 @@ def make_api_sig(params):
 def get_frob():
     ''' Returns *frob*. Part of the authentication process.'''
 
-    params = {'method':'rtm.auth.getFrob', 'api_key':API_KEY, 'format':'json'}
-
+    params = {'method':'rtm.auth.getFrob',
+              'api_key':API_KEY,
+              'format':'json'}
     # create signature from existing params and add as parameter
     params['api_sig'] = make_api_sig(params)
 
@@ -66,9 +77,9 @@ def get_frob():
         raise Exception("Error ({}) connecting to Remember the Milk. Please "
                         "try again later.".format(r.status_code))
 
-    frob = r.json()['rsp']['frob']
+    data = get_data_or_raise_exception(r)
 
-    return frob
+    return data['frob']
 
 
 def authenticate():
@@ -84,13 +95,17 @@ def authenticate():
               'perms':'read'}
     params['api_sig'] = make_api_sig(params)
 
-    r_authentication = requests.get(auth_url, params=params)
+    r = requests.get(auth_url, params=params)
+
+    if r.status_code != 200:
+        raise Exception("Error ({}) connecting to Remember the Milk. Please "
+                        "try again later.".format(r.status_code))
 
     click.echo('')
     click.echo('Open the following link in your browser in order to approve authentication '
         'from Remember the Milk:')
     click.echo('')
-    click.echo(r_authentication.url)
+    click.echo(r.url)
 
     # pause the application while the user approves authentication
     value = click.prompt('Type "c" and then press enter to continue.')
@@ -100,15 +115,17 @@ def authenticate():
               'method':'rtm.auth.getToken',
               'format':'json',
               'frob':frob}
+
     # sign every request
     params['api_sig'] = make_api_sig(params)
 
-    r_token = requests.get(methods_url, params=params)
+    r = requests.get(methods_url, params=params)
 
+    r = r.json()['rsp']
     #r_token.json()
-    settings['token'] = r_token.json()['rsp']['auth']['token']
-    settings['username'] = r_token.json()['rsp']['auth']['user']['username']
-    settings['name'] = r_token.json()['rsp']['auth']['user']['fullname']
+    settings['token'] = r['auth']['token']
+    settings['username'] = r['auth']['user']['username']
+    settings['name'] = r['auth']['user']['fullname']
     save(settings)
 
     click.echo('Congrats, {}, it worked!'.format(settings['name']))
@@ -119,7 +136,7 @@ def authenticate():
 def check_token():
     ''' Check user token. Do nothing, refresh token, or authenticate. '''
 
-    if settings['token']:
+    if 'token' in settings:
         params = {'api_key':API_KEY,
                   'method': 'rtm.auth.checkToken',
                   'format':'json',
@@ -145,7 +162,9 @@ def check_token():
 
     return
 
-
+################################################################################
+# commands
+################################################################################
 @click.group()
 def main():
     pass
@@ -160,9 +179,8 @@ def test():
 @main.command()
 def get_completed_tasks():
     check_token()
-    click.echo('Token good.')
-    params = {'method':'rtm.tasks.getList',
-              'api_key':API_KEY,
+    params = {'api_key':API_KEY,
+              'method':'rtm.tasks.getList',
               'format':'json',
               'auth_token':settings['token'],
               'filter':'completed:today'}
@@ -170,7 +188,11 @@ def get_completed_tasks():
 
     r = requests.get(methods_url, params=params)
 
-    print(r.json()['rsp']['tasks'])
+    tasks = get_data_or_raise_exception(r)['tasks']
+
+    print(tasks)
+
+    return
 
 
 @main.command()
